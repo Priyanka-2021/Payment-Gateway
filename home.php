cbe5121d5cf3c80d5b7765387b38f95f5b3c2dca
Priyanka-2021 <84039926+Priyanka-2021@users.noreply.github.com>
2021-05-19 13:41:52 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>HOME Payment Gateway</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+            <a type="button" href="home.php" class="btn btn" style="margin-right: 8px; background-color:gray; color:#fff;" >Home</a>
+            <a type="button" href="https://www.thesparksfoundationsingapore.org/" class="btn btn-info" style= "margin-right : 8px;background-color:#28b5b5;" target="_blank">About us</a>
+            <a type="button" href="donate.php" class="btn btn-danger" style= "margin-right:  8px">Donate</a>
+      </div>
+    </div>
+    <div class="top" style="color:#fff;">
+      <h1 >Covid Donation Cell</h1>
+      <p>We will get through this together!!</p>
+    </div>
+    </nav>
+<img src="donate.jpg" id="image">
+    <div class="container">
+<h1 style="text-align:center; margin-top:7%;text-transform:uppercase;color:#34656d;font-family:Algerian; font-size:42px;">Your contribution can help those negatively impacted by covid-19 virus. </h1>
+    <p style="margin-top:440px; font-size:18px;">  As the COVID-19 crisis continues to loom over us, The
+Government of India has taken a strict combat measures by directing a
+lockdown. At this great home of need, in close coodination with state Governments
+and District administration has stepped to provide relief by providing food, beds and oxygens
+supply to thousands of people across the country.</p>
+<p  style=" font-size:18px;margin-bottom: 20px; padding-bottom:40px;">
+ &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp  By adhering to all safety and hygiene measures, we have begun its relief
+service by providing meal or packed grocery kits to the marginalised and low-income segment of the society comprising of daily wage workers, migrant labours,
+construction site workers and needy people at old age home and night shelters, We are providing bed and oxygen
+supply to the District administration so that needy people will get this as soon as possible.
+    <p>
+      <p></p>
+    </div>
+
+          <footer class="footer" style="background-color:#4d4d4d">
+      <div class="footer-widget-section">
+          <div class="container text-center">
+              <div class="row">
+                  <div class="col-sm-4 footer-block">
+                      <div class="footer-widget widget_text">
+                          <div class="footer-logo">
+                          <br><br><a href="#"><img style="height: 70px;width:70px"src="give-love.png" alt=""></a>
+                          </div>
+                          <p style="color:#fff;"><br>Made By:Priyanka Patra</p>
+                          <p style="color:#fff;"><a href="#">#GRIPMAY2021</a> Web Development & Designing Internship</p>
+                      </div>
+                  </div>
+                  <div class="col-sm-4 footer-block">
+                      <div class="footer-widget widget_text">
+                      <br><br><h3   style="color:#fff;">About Us</h3>
+                          <p style="color:#fff;">This is a website for transferring money online safely</p><br>
+                      </div>
+                  </div>
+                  <div class="col-sm-4 footer-block last">
+                      <div class="footer-widget widget_text">
+                          <br><br><h3 style="color:#fff;">Contact Us Today</h3>
+                          <address style="color:#fff;">
+                              Call Us 959595-969696 OR 949494-989898<br>
+                              Send an Email on <a href="mailto:#">contact@basicbank.com</a><br>
+                              Visit Us 123 Fake Street- tola 12358<br>
+                              India
+                          </address>
+                  </div>
+              </div>
+          </div>
+      </div>
+  <script src="js/jquery-2.1.4.min.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="js/smoothscroll.js"></script>
+  <script src="js/mobile-menu.js"></script>
+  <script src="js/flexSlider/jquery.flexslider-min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js"></script>
+  <script src="js/scripts.js"></script>
+  </body>
+</html>
+<style>
+#image{
+  height:100%;
+  width: 100%;
+  position:absolute;
+  z-index:-1;
+  opacity: 0.9;
+
+}
+.footer {
+  .footer-widget{
+    color: #dcdcdc;
+    font-size: 13.5px;
+}
+
+}
+</style>
